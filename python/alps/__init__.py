709ed409ef015a01f4e275a1070e38cf3c96a427
Alexander <galexv@umich.edu>
2016-02-09 13:44:08 -0500

    Build Python source module by `make`.

---
+++ python/alps/__init__.py
@@ -0,0 +1,3 @@
+# Does nothing.
+
+# print "__init__.py in alps has been called."
---
+++ python/alps/hdf5.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# Loader for ALPS HDF5 module
+
+from .pyhdf5_c import hdf5_archive_impl as archive
